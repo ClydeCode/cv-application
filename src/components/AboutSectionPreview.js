4263ab2eeeb360a048f9c350d578671b0b576f69
Clyde <noneclyde@gmail.com>
2022-09-13 15:04:28 +0300

    Final version of AboutSectionPreview component

--- src/components/AboutSectionPreview.js
+++ src/components/AboutSectionPreview.js
@@ -9,14 +9,20 @@ export default class AboutSectionPreview extends React.Component {
                 <div className='about-preview-photo'><img src={image} alt='' /></div>
                 <div className='about-preview-otherside'>
                     <div className='about-preview-name'><b>James</b> Watson</div>
-                    <div className='about-preview-address'>
-                        956, 31st Street 
-                        NYC - 10001 
-                        United States
+                    <div className='about-preview-container'>
+                        <div className='about-preview-left'>
+                            <div className='about-preview-address'>
+                                956, 31st Street 
+                                NYC - 10001 
+                                United States
+                            </div>
+                        </div>
+                        <div className='about-preview-right'>
+                            <div className='about-preview-email'><b>E-mail:</b> james.w@thecompany.com</div>
+                            <div className='about-preview-website'><b>Website:</b> https://makelife.com</div>
+                            <div className='about-preview-phone'><b>Phone:</b> (456) 789 125</div>
+                        </div>
                     </div>
-                    <div className='about-preview-email'>james.w@gmail.com</div>
-                    <div className='about-preview-website'>https://makelife.com</div>
-                    <div className='about-preview-phone'>(456) 789 125</div>
                 </div>
             </div>
         )
