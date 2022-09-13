a4a0079a5ac57d6dabcb38ed2a394268d827834a
Clyde <noneclyde@gmail.com>
2022-09-13 15:48:49 +0300

    Add final version of Section component

---
+++ src/components/Section.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/section.css';
+
+export default class Section extends React.Component {
+    render() {
+        return (
+            <div className='section'>
+                <div className='section-container'>
+                    <div className='section-title-container'>
+                        <div className='section-title'><b>COMPUTER SKILLS</b></div>
+                    </div>
+                    <div className='section-content'>
+                    Microsoft Word, Excel, Access, PowerPoint, Outlook Express, Microsoft
+                    Windows XP and Microsoft Office XP Professional
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
