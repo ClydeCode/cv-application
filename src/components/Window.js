e893e5ce1c159325db6e82083b32a3b977bf2bdf
Clyde <noneclyde@gmail.com>
2022-09-27 14:55:23 +0300

    Rename files

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutSectionPreview from './AboutSectionPreview';
+import Section from './Section'
 import '../styles/window.css';
 
 export default class PreviewWindow extends React.Component {
     render() {
         return (
-            <div className='window' id='preview-window'>
+            <div className='window'>
                 <AboutSectionPreview />
+                <Section />
             </div>
         )
     }
